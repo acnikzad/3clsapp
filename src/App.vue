bed1d324c6a21d3539d50f26c96a49b0212b4a3d
“Aria <a.c.nikzad@gmail.com>
2020-03-30 15:59:18 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,138 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <head>
+    <title>Elements - Phantom by HTML5 UP</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="/assets/css/main.css" />
+    <noscript><link rel="stylesheet" href="/assets/css/noscript.css" /></noscript>
+  </head>
+  <body>
+<!-- Wrapper -->
+      <div id="wrapper">
+<!-- Header -->
+          <header id="header">
+            <div class="inner">
+<!-- Logo -->
+                <a href="index.html" class="logo">
+                  <span class="symbol"><img src="images/CLS Logo Only Graphic.jpg" alt="" /></span><span class="title">Concrete Landscape Services</span>
+                </a>
+<!-- Nav -->
+                <nav>
+                  <ul>
+                    <li><a href="#menu">Menu</a></li>
+                  </ul>
+                </nav>
+            </div>
+          </header>
+<!-- Menu -->
+          <nav id="menu">
+            <h2>Menu</h2>
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="generic.html">Ipsum veroeros</a></li>
+              <li><a href="generic.html">Tempus etiam</a></li>
+              <li><a href="generic.html">Consequat dolor</a></li>
+              <li><a href="elements.html">Elements</a></li>
+            </ul>
+          </nav>
+<!-- Main -->
+        <div id="main">
+          <div class="inner">
+            <header>
+              <h1>Concrete, landscape, repair; we do it all!</h1>
+              <p>Concrete Landscape Services specialize in high quality commercial and residential applications for any need. We are dedicated to providing you with services by friendly trained professionals, who understand the importance of both safety and excellent customer services</p>
+            </header>
+            <section class="tiles">
+              <article class="style3">
+                <span class="image">
+                  <img src="images/Driveway, rv, entry.jpg" alt="" />
+                </span>
+                <a href="generic.html">
+                  <h2>Residential</h2>
+                  <div class="content">
+                    <p>From grading your site for construction, to bringing your ideas to life, the experienced team of CLS will work with you to make your dream a reality.</p>
+                  </div>
+                </a>
+              </article>
+              <article class="style2">
+                <span class="image">
+                  <img src="images/pic02.jpg" alt="" />
+                </span>
+                <a href="generic.html">
+                  <h2>Commercial</h2>
+                  <div class="content">
+                    <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+                  </div>
+                </a>
+              </article>
+              <article class="style3">
+                <span class="image">
+                  <img src="images/Broom stain driveway.jpg" alt="" />
+                </span>
+                <a href="generic.html">
+                  <h2>Repair</h2>
+                  <div class="content">
+                    <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+                  </div>
+                </a>
+              </article>
+            </section>
+          </div>
+        </div>
+<!-- Footer -->
+        <footer id="footer">
+          <div class="inner">
+            <section>
+              <h2>Get in touch</h2>
+              <form method="post" action="#">
+                <div class="fields">
+                  <div class="field half">
+                    <input type="text" name="name" id="name" placeholder="Name" />
+                  </div>
+                  <div class="field half">
+                    <input type="email" name="email" id="email" placeholder="Email" />
+                  </div>
+                  <div class="field">
+                    <textarea name="message" id="message" placeholder="Message"></textarea>
+                  </div>
+                </div>
+                <ul class="actions">
+                  <li><input type="submit" value="Send" class="secondary" /></li>
+                </ul>
+              </form>
+            </section>
+            <section>
+              <h2>Follow</h2>
+              <ul class="icons">
+                <li><a href="#" class="icon brands style2 fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="#" class="icon brands style2 fa-facebook-f"><span class="label">Facebook</span></a></li>
+                <li><a href="#" class="icon brands style2 fa-instagram"><span class="label">Instagram</span></a></li>
+                <li><a href="#" class="icon brands style2 fa-dribbble"><span class="label">Dribbble</span></a></li>
+                <li><a href="#" class="icon brands style2 fa-github"><span class="label">GitHub</span></a></li>
+                <li><a href="#" class="icon brands style2 fa-500px"><span class="label">500px</span></a></li>
+                <li><a href="#" class="icon solid style2 fa-phone"><span class="label">Phone</span></a></li>
+                <li><a href="#" class="icon solid style2 fa-envelope"><span class="label">Email</span></a></li>
+              </ul>
+            </section>
+            <ul class="copyright">
+              <li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            </ul>
+          </div>
+        </footer>
+      </div>
+    </body>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
 }
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/home.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="home">
+<!-- Main -->
+      <div class="container">
+        <div id="main">
+          <div class="inner">
+            <header>
+              <h1>Concrete Landscape Services</h1>
+              <p>Concrete Landscape Services specialize in high quality commercial and residential applications for any need. We are dedicated to providing you with services by friendly trained professionals, who understand the importance of both safety and excellent customer services</p>
+            </header>
+            <section class="tiles">
+              <article class="style3">
+                <span class="image">
+                  <img src="images/Driveway, rv, entry.jpg" alt="" />
+                </span>
+                <a href="generic.html">
+                  <h2>Residential</h2>
+                  <div class="content">
+                    <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+                  </div>
+                </a>
+              </article>
+              <article class="style2">
+                <span class="image">
+                  <img src="images/pic02.jpg" alt="" />
+                </span>
+                <a href="generic.html">
+                  <h2>Commercial</h2>
+                  <div class="content">
+                    <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+                  </div>
+                </a>
+              </article>
+              <article class="style3">
+                <span class="image">
+                  <img src="images/Broom stain driveway.jpg" alt="" />
+                </span>
+                <a href="generic.html">
+                  <h2>Repair</h2>
+                  <div class="content">
+                    <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+                  </div>
+                </a>
+              </article>
+            </section>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>;
+
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
